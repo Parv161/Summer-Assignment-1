bb2d8ffe822e4c19a7350bd6547004f9a8b2ff77
parv vats <parvsinghvats@parvs-MacBook-Air.local>
2026-06-15 23:31:19 +0530

    added Day-9 code files

---
+++ Day-9/c3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j <= i; j++) {
+            printf("%c", 'A' + i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
